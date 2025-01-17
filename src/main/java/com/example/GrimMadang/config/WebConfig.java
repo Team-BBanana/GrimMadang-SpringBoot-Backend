b33388580eb85f5abeabe070fa52ac21277c0518
develsvai <developsvai5096@gmail.com>
2025-01-17 11:39:33 +0000

    Refactor : 도커파일 수정

--- src/main/java/com/example/GrimMadang/config/WebConfig.java
+++ src/main/java/com/example/GrimMadang/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(@NonNull CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://grim-madang.store") // 허용할 도메인
+                .allowedOrigins("http://grim-madang.store") // 허용할 도메인
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("Content-Type", "*")
                 .allowCredentials(true);
